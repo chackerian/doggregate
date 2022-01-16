0ea377a59e21edb533ed6862573cb6d00c26cf1c
Nathan <nchackerian@gmail.com>
2022-01-16 10:18:45 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
+
+function Dogs(props) {
+  console.log("RESULTS", props.resultsArr)
+  return (
+      <div className="listings">
+        {props.resultsArr.map(({ age, sex, breed, image}) => (
+            <div className="dogListing">
+              <img src={image} alt="" />
+              <span>Age: {age}</span>
+              <br />
+              <span>Breed: {breed}</span>
+              <br />
+              <span>Gender: {sex}</span>
+            </div>
+        ))}
+      </div>
+  );
+}
 
 class App extends Component {
 
@@ -7,7 +25,7 @@ class App extends Component {
     super(props)
     this.state = {
       zip: '',
-      resultsArr: []
+      resultsArr: [],
     }
   }
 
@@ -15,34 +33,39 @@ class App extends Component {
     this.setState({
       zip: event
     })
-    console.log(this.state)
   }
 
   searchy = () => {
-    console.log(this.state)
     var xhttp = new XMLHttpRequest();
-    var that = this.state.zip
+    var zip = this.state.zip
     xhttp.open("GET", "https://data.austintexas.gov/resource/vwti-2wcd.json", true);
+    var results = []
     xhttp.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         var parse = JSON.parse(this.responseText);
-        var arr2 = parse.filter(x => x["location_zip"] === that)
+        var arr2 = parse.filter(x => x["location_zip"] === zip)
 
         arr2.map(x => {
           var id = x.animal_id
           var age = x.age
           var sex = x.sex
+          var breed = x.looks_like
+          console.log(x)
           var url = "http://petharbor.com/get_image.asp?RES=Detail&ID=" + id + "&LOCATION=ASTN"
           x.image = url
 
-          var newarr = {age: age, sex: sex, image: url}
-          resultsArr.push(newarr)
+          var newarr = {age: age, sex: sex, breed: breed, image: url}
 
           console.log("NEW ARRAY", newarr)
+          results.push(newarr)
         })
 
       }
     };
+    this.setState({
+      resultsArr: results
+    })
+    console.log("QUERY", results, this.state)
     xhttp.send();
   }
 
@@ -50,32 +73,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="App-title">Doggregate</h1>
-        <input type="text" className="searchbar" placeholder="Enter a zip code" value={this.state.zip} onChange={(e) => this.inputy(e.target.value)} />
-        <button class="search" onClick={() => this.searchy()}>Search</button>
-        <p className="results" id="results"></p>
-        <Dogs {...this} />
+        <div className="searchdiv">
+          <input type="text" className="searchbar" placeholder="Enter a zip code" value={this.state.zip} onChange={(e) => this.inputy(e.target.value)} />
+          <button className="search" onClick={() => this.searchy()}>Search</button>
+        </div>
+        <Dogs resultsArr={this.state.resultsArr} />
       </div>
     );
   }
-}
-
-class Dogs extends Component {
-    
-    constructor(props) {
-        super(props);    
-        console.log(props, this)   
-    }
-    
-    render() {
-        
-        return (
-            <div className="dogListing">
-            <div id="dog">Age: {newarr.age}</div>
-            <div id="dog">Sex: {newarr.sex}</div>
-            <img src={newarr.image} alt="" />
-            </div>
-        );
-    }
 }
 
 export default App;
